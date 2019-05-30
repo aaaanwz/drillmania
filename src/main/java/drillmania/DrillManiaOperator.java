2fac669ae8ecbc24995d60473235e1f356a7c2d4
takao-anazawa <takao-anazawa@bebit.com>
2019-05-30 11:53:48 +0900

    correspond to v1.0.1

--- src/main/java/drillmania/DrillManiaOperator.java
+++ src/main/java/drillmania/DrillManiaOperator.java
@@ -2,7 +2,8 @@ package drillmania;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -11,91 +12,90 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DrillManiaOperator implements AutoCloseable {
-	private WebDriver driver;
-	final int retryNum = 100;
+  private WebDriver driver;
+  final int retryNum = 100;
 
-	public DrillManiaOperator() {
-		ChromeDriverLoader.init();
-		this.driver = new ChromeDriver();
-		driver.get("https://drillmania.work");
-	}
+  public DrillManiaOperator() {
+    ChromeDriverLoader.init();
+    this.driver = new ChromeDriver();
+    driver.get("https://drillmania.work");
+  }
 
-	/**
-	 * @return true if there is no gameplay remaining time
-	 */
-	public boolean isTimeOver() {
-		try {
-			driver.findElement(By.className("result"));
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
+  /**
+   * @return true if the game is started
+   */
+  public boolean isStarted() {
+    return driver.findElement(By.className("start-count")).getAttribute("style")
+        .equals("display: none;");
+  }
 
-	/**
-	 * Determine the type of silhouette
-	 * 
-	 * @return DrillType
-	 * @throws MalformedURLException failed to parse url of the picture.
-	 * @throws InterruptedException
-	 */
-	public DrillType getDrillType() throws MalformedURLException, InterruptedException {
-		String imgSrc;
-		int count = 0;
-		while (true) {
-			try {
-				imgSrc = driver.findElement(By.className("black")).getAttribute("src");
-				return DrillType.getDrillType(new URL(imgSrc));
-			} catch (NoSuchElementException e) {
-				count++;
-				if (count > retryNum) {
-					throw new RuntimeException(e);
-				} else {
-					Thread.sleep(1);
-				}
-			}
-		}
-	}
+  /**
+   * @return true if there is no gameplay remaining time
+   */
+  public boolean isTimeOver() {
+    try {
+      driver.findElement(By.className("result"));
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 
-	/**
-	 * Click YES/NO button.
-	 * 
-	 * @param isDrill
-	 */
-	public void sendResult(boolean isDrill) {
-		String buttonClassName;
-		if (isDrill) {
-			buttonClassName = "yes";
-		} else {
-			buttonClassName = "no";
-		}
-		try {
-			driver.findElement(By.className(buttonClassName)).click();
-		} catch (WebDriverException e) {
-			//do nothing
-		}
-	}
+  /**
+   * Determine the type of silhouette
+   * 
+   * @return DrillType
+   * @throws MalformedURLException failed to parse url of the picture.
+   * @throws InterruptedException
+   */
+  public Boolean isDrill() throws MalformedURLException, InterruptedException {
+    return driver.findElements(By.className("black"))
+        .stream()
+        .filter(e -> !e.getAttribute("style").equals("display: none;"))
+        .map(e -> {
+          try {
+            return new URL(e.getAttribute("src")).getPath();
+          } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
+          }
+        })
+        .map(e -> drillPictureMap.get(e))
+        .findAny()
+        .orElse(null);
+  }
 
-	enum DrillType {
-		REALDRILL("/_nuxt/img/3040568.png"), 
-		ANIMEDRILL("/_nuxt/img/678a65b.png"), 
-		TAKENOKO("/_nuxt/img/53493b5.png"),
-		ASPARAGUS("/_nuxt/img/1a79a5b.png");
+  final static Map<String, Boolean> drillPictureMap = new HashMap<String, Boolean>();
+  static {
+    drillPictureMap.put("/_nuxt/img/a565676.png", true);// REALDRILL
+    drillPictureMap.put("/_nuxt/img/cdc53ee.png", true);// ANIMEDRILL
+    drillPictureMap.put("/_nuxt/img/87ce995.png", false);// TAKENOKO
+    drillPictureMap.put("/_nuxt/img/60911dc.png", false);// ASPARAGUS
+  }
 
-		private final String filename;
+  /**
+   * Click YES/NO button.
+   * 
+   * @param isDrill
+   */
+  public void sendResult(Boolean isDrill) {
+    if (isDrill == null) {
+      return;
+    }
+    String buttonClassName;
+    if (isDrill) {
+      buttonClassName = "yes";
+    } else {
+      buttonClassName = "no";
+    }
+    try {
+      driver.findElement(By.className(buttonClassName)).click();
+    } catch (WebDriverException e) {
+      // do nothing
+    }
+  }
 
-		private DrillType(final String filename) {
-			this.filename = filename;
-		}
-
-		public static DrillType getDrillType(URL url) {
-			return Stream.of(values()).filter(pictures -> pictures.filename.equals(url.getPath())).findAny()
-					.orElse(null);
-		}
-	}
-
-	@Override
-	public void close() throws Exception {
-		driver.quit();
-	}
+  @Override
+  public void close() throws Exception {
+    driver.quit();
+  }
 }
--- src/main/java/drillmania/Main.java
+++ src/main/java/drillmania/Main.java
@@ -1,19 +1,17 @@
 package drillmania;
 
-import drillmania.DrillManiaOperator.DrillType;
-
 public class Main {
-	public static void main(String[] args) throws Exception {
-		try(DrillManiaOperator oper=new DrillManiaOperator()){
-			while (!oper.isTimeOver()) {
-				DrillType drillType = oper.getDrillType();
-				if (drillType.equals(DrillType.ANIMEDRILL) || drillType.equals(DrillType.REALDRILL)) {
-					oper.sendResult(true);
-				} else {
-					oper.sendResult(false);
-				}
-			}
-			Thread.sleep(10000);//enjoy your sroce!
-		}
-	}
+  public static void main(String[] args) throws Exception {
+    try (DrillManiaOperator oper = new DrillManiaOperator()) {
+      while (!oper.isStarted()) {
+        // wait
+      }
+
+      while (!oper.isTimeOver()) {
+        Boolean isDrill = oper.isDrill();
+        oper.sendResult(isDrill);
+      }
+      Thread.sleep(10000); // enjoy your score!
+    }
+  }
 }
